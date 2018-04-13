b6b7089111906d95661e077a841e932c90be971f
Fredrik Teschke <f@ftes.de>
2018-04-13 23:19:25 +0200

    [feature] add form to add tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,21 +5,39 @@ import { withTracker } from 'meteor/react-meteor-data';
 import Tasks from '/imports/api/tasks';
 import Task from './Task';
 
-const App = ({ tasks }) => (
-  <div className="container">
-    <header>
-      <h1>Todo List</h1>
-    </header>
-    <ul>
-      {tasks.map(task => (
-        <Task key={task._id} task={task} />
-      ))}
-    </ul>
-  </div>
-);
+const App = ({ tasks }) => {
+  let textInput;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const text = textInput.value.trim();
+    Tasks.insert({ text, createdAt: new Date() });
+    textInput.value = '';
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Todo List</h1>
+
+        <form className="new-task" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Type to add new tasks"
+            ref={(c) => { textInput = c; }}
+          />
+        </form>
+      </header>
+      <ul>
+        {tasks.map(task => (
+          <Task key={task._id} task={task} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 App.propTypes = {
-  tasks: PropTypes.shape().isRequired,
+  tasks: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default withTracker(() => ({
